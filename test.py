c746b11faed7385eb8099771b40d449eaea9533a
Brian Liu <samleoqh@gmail.com>
2017-08-26 18:08:25 +0200

    basic pytorch code

---
+++ test.py
@@ -0,0 +1,48 @@
+"""
+This is a basic pytorch tutorial script
+source1: https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/01-basics/pytorch_basics/main.py
+source2:https://github.com/MorvanZhou/PyTorch-Tutorial/blob/master/tutorial-contents/202_variable.py
+
+#Qingui Liu
+#26/08/2017
+"""
+import torch
+import torchvision
+import torch.nn as nn
+import numpy as np
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+from torch.autograd import Variable
+
+#basic examples 1
+#tensors
+x = Variable(torch.Tensor([1]),requires_grad=True)
+w = Variable(torch.Tensor([2]),requires_grad=True)
+b = Variable(torch.Tensor([3]),requires_grad=True)
+
+y = w * x + b
+
+# gradients.
+y.backward()
+
+# print(x.grad)
+# print(w.grad)
+# print(b.grad)
+#convert numpy to tensor, vise versa
+np_data = np.arange(6).reshape(2,3)
+torch_data = torch.from_numpy(np_data)
+tesnsor2array = torch_data.numpy()
+# print ('\n numpy array:',np_data,
+#        '\n troch tensor:', torch_data,
+#        '\n tensor to arr:',tesnsor2array )
+
+data = [-1, -2, -3, 3]
+tensor = torch.FloatTensor(data)
+#sin, mean
+# print (np.sin(data), '\n', torch.sin(tensor))
+# print (np.mean(data), '\n', torch.mean(tensor))
+
+a = [[1,2],[3,2]]
+b = torch.FloatTensor(a)
+# print (np.matmul(a,a), torch.mm(b,b), a,b)
