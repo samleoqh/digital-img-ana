9325a55bbea9ded8f18b6e7449d14740d09d36f9
Brian Liu <samleoqh@gmail.com>
2017-09-06 20:04:03 +0200

    wrapping functions into a class

--- exercise/patchExtract.py
+++ exercise/patchExtract.py
@@ -5,12 +5,12 @@ image patch extracting with sliding windows
 labeled images will be automatically converted to gray-scaled image
 following below rules.
 
-Building            (RGB: 0, 0, 255)    ---> 1 (b001 -1)
-Tree                (RGB: 0, 255, 0)    ---> 2 (b010 -1)
-Low vegetation      (RGB: 0, 255, 255)  ---> 3 (b011 -1)
-Clutter/background  (RGB: 255, 0, 0)    ---> 4 (b100 -1)
-Impervious surfaces (RGB: 255, 255, 255)---> 7 (b111 -3)
-Car                 (RGB: 255, 255, 0)  ---> 6 (b110 -1)
+Building            (RGB: 0, 0, 255)    ---> 1  or 0(b001 -1)
+Tree                (RGB: 0, 255, 0)    ---> 2  or 1(b010 -1)
+Low vegetation      (RGB: 0, 255, 255)  ---> 3  or 2(b011 -1)
+Clutter/background  (RGB: 255, 0, 0)    ---> 4  or 3(b100 -1)
+Impervious surfaces (RGB: 255, 255, 255)---> 7  or 4(b111 -3)
+Car                 (RGB: 255, 255, 0)  ---> 6  or 5(b110 -1)
 
 there are no '5' - b101 and '0' -b000,
 """
@@ -20,6 +20,7 @@ import os
 from imutils import paths
 import scipy.misc
 
+
 def main():
     """
     test the patch_extract function
@@ -28,68 +29,93 @@ def main():
     # initialize some parameters needed by patch_extract function
     dir = "./images/remotesensing"  # image home folder
     (ph, pw) = (256, 256)  # patch size
-    step = 200  # sliding step
+    step = 5800  # sliding step
 
-    total_patch = patch_extract(input_dir=dir, patchSzie=(ph,pw), stepSize=step)
-    print("total patches: {0}".format(total_patch))
+    myDataset = ImageData(dir)
+    print(myDataset)
+
+    total_patch = myDataset.patch_extract(patchSize=(ph, pw), stepSize=step)
+    print("Total number of patches extracted is : {0}".format(total_patch))
 
 
-def patch_extract(input_dir, patchSize=(256,256), stepSize=200):
-    """
-    :param input_dir: path to your image folder
-    :param patchSize: patch size - height and width
-    :param stepSize: sliding step, should less than patch size
-    :return: total patch numbers extracted successfully
-    """
-    ph = patchSize[0] # patch height
-    pw = patchSize[1] # patch width
-    total_patch = 0
-    for _, imgPath in enumerate(paths.list_images(input_dir)):
-        path, filename = os.path.split(imgPath)  # filename = imgPath.split(os.path.sep)[-1]
-        suffix = os.path.splitext(os.path.basename(filename))[0] + ".png"
-        img = cv2.imread(imgPath)
+class ImageData:
+    def __init__(self, path="./image"):
+        self.folder = path
+        self.num_files = sum([len(files) for r, d, files in os.walk(self.folder)])
 
-        for x in range(0, img.shape[0], stepSize):
-            px = x
-            endX = px + ph
-            if endX > img.shape[0]:
-                x = endX
-                endX = img.shape[0]
-                px = endX - ph
-            else:
-                x += stepSize
-                y = 0
+    def __str__(self):
+        return "The path to the data set is '%s' which has total %d files" % (self.folder,self.num_files)
 
-            for y in range(0, img.shape[1], stepSize):
-                py = y
-                endY = py + pw
-                if endY > img.shape[1]:
-                    y = endY
-                    endY = img.shape[1]
-                    py = endY - pw
+    def __setattr__(self, key, value):
+        self.__dict__[key] = value
+
+    @classmethod
+    def rgb2gray(cls,rgb_img):
+        assert len(rgb_img.shape) == 3, 'RGB images only'
+        gray_img = rgb_img[:, :, 2] / 255 + \
+                   (rgb_img[:, :, 1] / 255) * 2 + \
+                   (rgb_img[:, :, 0] / 255) * 4
+        return gray_img
+
+    #@classmethod
+    def patch_extract(self, patchSize=(256, 256), stepSize=200):
+        """
+        :param patchSize: patch size - height and width
+        :param stepSize: sliding step, should less than patch size
+        :return: total patch numbers extracted successfully
+        Note: the generated patch files will be stored as new .png files
+         in the same folder with the original file
+        """
+        ph = patchSize[0]  # patch height
+        pw = patchSize[1]  # patch width
+        total_patch = 0
+        for _, imgPath in enumerate(paths.list_images(self.folder)):
+            path, filename = os.path.split(imgPath)  # filename = imgPath.split(os.path.sep)[-1]
+            suffix = os.path.splitext(os.path.basename(filename))[0] + ".png"
+            img = cv2.imread(imgPath)
+
+            for x in range(0, img.shape[0], stepSize):
+                px = x
+                endX = px + ph
+                if endX > img.shape[0]:
+                    x = endX
+                    endX = img.shape[0]
+                    px = endX - ph
                 else:
-                    y += stepSize
+                    x += stepSize
+                    y = 0
 
-                patch = img[px:endX, py:endY]
+                for y in range(0, img.shape[1], stepSize):
+                    py = y
+                    endY = py + pw
+                    if endY > img.shape[1]:
+                        y = endY
+                        endY = img.shape[1]
+                        py = endY - pw
+                    else:
+                        y += stepSize
 
-                # you can change the new filename format as you please
-                newName = "patch_" + str(px) + "_" + str(py) + "_" + suffix
+                    patch = img[px:endX, py:endY]
 
-                # exam if it is labeled images by its filename containing 'label' str
-                if "label" in filename:
-                    # mapping 3-channel RGB value to a gray scalar to form a gray image
-                    newPatch = patch[:, :, 2] / 255 + \
-                               (patch[:, :, 1] / 255) * 2 + \
-                               (patch[:, :, 0] / 255) * 4
+                    # you can change the new filename format as you please
+                    newName = "patch_" + str(px) + "_" + str(py) + "_" + suffix
 
-                    scipy.misc.imsave(os.path.join(path, newName),
-                                      newPatch)  # don't know how to use cv2 to save as a gray file
-                else:
-                    cv2.imwrite(os.path.join(path, newName), patch)
+                    # exam if it is a labeled image by its filename containing 'label' str
+                    if "label" in filename:
+                        # mapping 3-channel RGB values to a gray scalar to form a new grayscale image
+                        # newPatch = patch[:, :, 2] / 255 + \
+                        #            (patch[:, :, 1] / 255) * 2 + \
+                        #            (patch[:, :, 0] / 255) * 4
+                        newPatch = ImageData.rgb2gray(patch)
 
-                total_patch += 1
+                        scipy.misc.imsave(os.path.join(path, newName),
+                                          newPatch)  # don't know how to use cv2 to save as a gray file
+                    else:
+                        cv2.imwrite(os.path.join(path, newName), patch)
 
-    return total_patch
+                    total_patch += 1
+
+        return total_patch
 
 
 if __name__ == "__main__":
